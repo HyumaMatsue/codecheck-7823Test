053c67c34a9bb98959d8ec415997556d6f85d8fe
Hyuma Matsue <Hyuma Matsue>
2017-03-22 00:00:44 +0900

    test_commit

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,7 +2,7 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-      System.out.println("test Log Hello World");
+      System.out.println("test Hello World");
 		for (int i = 0, l = args.length; i < l; i++) {
 			System.out.println(args[i]);
 		}
