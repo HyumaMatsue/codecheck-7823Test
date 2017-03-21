859d4e1a65d3b1ac1692c6d8c894acff7f2d1e3c
code-project <dev@code-check.io>
2017-03-21 14:52:58 +0000

    Initial commit

---
+++ src/main/java/codecheck/App.java
@@ -0,0 +1,10 @@
+package codecheck;
+
+public class App {
+	public static void main(String[] args) {
+      System.out.println("test Log Hello World");
+		for (int i = 0, l = args.length; i < l; i++) {
+			System.out.println(args[i]);
+		}
+	}
+}
